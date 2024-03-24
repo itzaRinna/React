d83e4af7ca56f20ec2003c53fdee111f997b882d
itzaRinna <dutchismahdick@gmail.com>
2024-03-24 13:06:24 +0700

    new push bitch

--- Bro Code/6/6/src/List.jsx
+++ Bro Code/6/6/src/List.jsx
@@ -1,21 +1,51 @@
+import PropTypes from 'prop-types';
 
-function List(){
-    const fruits = [{id:1,name:"apple", calories: 95},
+function List(props){
+    /*const fruits = [{id:1,name:"apple", calories: 95},
                     {id:2,name:"orange", calories:45},
                     {id:3,name:"banana",calories:105},
                     {id:4,name:"coconut",calories:159},
-                    {id:5,name:"pineapple", calories: 37}];
+                    {id:5,name:"pineapple", calories: 37}];*/
+    const category = props.category;
+    const itemList = props.items;
 
     //fruits.sort((a, b) => a.name.localeCompare(b.name)); //ALPHABETICAL
     //fruits.sort((a, b) => b.name.localeCompare(a.name)); //REVERSE ALPHABETICAL
     //fruits.sort((a, b) => a.calories - b.calories); //NUMERIC
-    fruits.sort((a, b) => b.calories - a.calories); //REVERSE NUMERIC
+    //fruits.sort((a, b) => b.calories - a.calories); //REVERSE NUMERIC
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
+    //const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
+    //const highCalFruits = fruits.filter(fruit => fruit.calories >= 100)
+
+
+    /*const listItems = fruits.map(fruit => <li key={fruit.id}>
                                             {fruit.name}: &nbsp;
-                                            <b>{fruit.calories}</b></li>);
+                                            <b>{fruit.calories}</b></li>);*/
 
-    return(<ol>{listItems}</ol>);
+    /*const listItems = lowCalFruits.map(lowCalFruits => <li key={lowCalFruits.id}>
+        {lowCalFruits.name}: &nbsp;
+        <b>{lowCalFruits.calories}</b></li>);*/
+    /*const listItems = highCalFruits.map(highCalFruits => <li key={highCalFruits.id}>
+        {highCalFruits.name}: &nbsp;
+        <b>{highCalFruits.calories}</b></li>);*/
+
+    const listItems = itemList.map(item => (<li key={item.id}>
+        {item.name}: &nbsp;
+        <b>{item.calories}</b></li>));
+
+    return(<>
+        <h3 className="list-category">{category}</h3>
+        <ol className="list-items">{listItems}</ol>
+    </>);
 }
-
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id:PropTypes.number,
+                                              name: PropTypes.string,
+                                              calories: PropTypes.number})),
+}
+List.defaultProps = {
+    category: "Category",
+    items: [],
+}
 export default List
